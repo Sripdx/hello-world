8c588dde7a8ac220f88b09322be0e1a19a319929
Sripdx <bonthalas@yahoo.com>
2017-05-06 09:43:55 -0700

    Hello World Java
    created Hello World Java class

---
+++ helloworld/src/extracts/common/HelloWorld.java
@@ -0,0 +1,13 @@
+package extracts.common;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello to world of GIT: " + args.length);
+
+	}
+
+}
